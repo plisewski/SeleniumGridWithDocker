dcfb7291733c0a13f6c95766e54422ed34e5ffe4
Piotr Lisewski <p.lisewski@hotmail.com>
2021-09-22 00:30:15 +0200

    initialize project structure

---
+++ SeleniumGridWithDocker/BaseClasses/TestBase.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumGridWithDocker.BaseClasses
+{
+    class TestBase
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
