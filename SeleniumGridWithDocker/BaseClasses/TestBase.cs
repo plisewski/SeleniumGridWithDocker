4a5312077e972d766dcfc96f015d6c105cd9c8dc
Piotr Lisewski <p.lisewski@hotmail.com>
2021-10-06 00:40:45 +0200

    implement TestBase class

--- SeleniumGridWithDocker/BaseClasses/TestBase.cs
+++ SeleniumGridWithDocker/BaseClasses/TestBase.cs
@@ -1,24 +1,55 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SeleniumGridWithDocker.BaseClasses
 {
-    class TestBase
+    public class TestBase
     {
+        protected RemoteWebDriver Driver;
+        private DriverOptions browserOptions;
+
         [SetUp]
         public void Setup()
         {
+            var hubUrl = TestContext.Parameters["RemoteWebDriverHubUrl"];
+            var browser = TestContext.Parameters["Browser"];
 
+            Driver = new RemoteWebDriver(new Uri(hubUrl), ChooseBrowserOptions(browser));
+
+            Driver.Manage().Window.Maximize();
         }
 
         [TearDown]
         public void TearDown()
         {
+            Driver.Quit();
+        }
 
+        private DriverOptions ChooseBrowserOptions(string browser)
+        {
+            switch (browser)
+            {
+                case "chrome":
+                    browserOptions = new ChromeOptions
+                    {
+                        BrowserVersion = "",
+                        PlatformName = "LINUX",
+                    };
+                    break;
+                case "firefox":
+                    browserOptions = new FirefoxOptions
+                    {
+                        BrowserVersion = "",
+                        PlatformName = "LINUX",
+                    };
+                    break;
+            }
+
+            return browserOptions;
         }
     }
 }
