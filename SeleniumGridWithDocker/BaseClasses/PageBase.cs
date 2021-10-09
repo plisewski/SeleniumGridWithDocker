c06eca731cca2f86ba49be47f302ce9d69bbe666
Piotr Lisewski <p.lisewski@hotmail.com>
2021-10-09 19:50:15 +0200

    implement common basic page operations

--- SeleniumGridWithDocker/BaseClasses/PageBase.cs
+++ SeleniumGridWithDocker/BaseClasses/PageBase.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Remote;
 
 namespace SeleniumGridWithDocker.BaseClasses
 {
-    class PageBase
+    abstract class PageBase
     {
+        protected readonly RemoteWebDriver Driver;
+
+        public PageBase(RemoteWebDriver remoteWebDriver)
+        {
+            Driver = remoteWebDriver;
+        }
+
+        public void ClickOnElement(By webElement)
+        {
+            Driver.FindElement(webElement).Click();
+        }
+
+        public void ClickOnElementUsingJavaScript(By webElement)
+        {
+            Driver.ExecuteScript("arguments[0].click();", Driver.FindElement(webElement));
+        }
+
+        public void ClearTextBox(By webElement)
+        {
+            if (Driver.FindElement(webElement).GetAttribute("value").Length > 0)
+                Driver.FindElement(webElement).Clear();
+        }
+
+        public void FillTextBox(By webElement, string text)
+        {
+            Driver.FindElement(webElement).SendKeys(text);
+        }
+
+        public void HoverOverElement(By webelement)
+        {
+            Actions action = new Actions(Driver);
+            action.MoveToElement(Driver.FindElement(webelement)).Perform();
+        }
     }
 }
