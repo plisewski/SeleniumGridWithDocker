2d1d42e486b2a0d429b119f95893c5638634b012
Piotr Lisewski <p.lisewski@hotmail.com>
2021-10-09 20:49:14 +0200

    add HomePage class

---
+++ SeleniumGridWithDocker/PageObjects/HomePage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium.Remote;
+using SeleniumGridWithDocker.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumGridWithDocker.PageObjects
+{
+    class HomePage : PageBase
+    {
+        public HomePage(RemoteWebDriver driver) : base(driver)
+        {
+
+        }
+
+        public HomePage Navigate()
+        {
+            Driver.Navigate().GoToUrl("http://automationpractice.com");
+
+            return this;
+        }
+    }
+}
