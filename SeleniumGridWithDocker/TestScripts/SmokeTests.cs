47dc52575cbe924ac8fbcb0a1739113e005fcbcb
Piotr Lisewski <p.lisewski@hotmail.com>
2021-10-09 21:01:17 +0200

    add CanNavigateToHomePage test

---
+++ SeleniumGridWithDocker/TestScripts/SmokeTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using SeleniumGridWithDocker.BaseClasses;
+using SeleniumGridWithDocker.PageObjects;
+
+namespace SeleniumGridWithDocker.TestScripts
+{
+    [TestFixture]
+    class SmokeTests : TestBase
+    {
+        [Test]
+        public void CanNavigateToHomePage()
+        {
+            var homePage = new HomePage(Driver);
+
+            homePage
+                .Navigate();
+
+            Assert.That(Driver.Url, Is.EqualTo("http://automationpractice.com/index.php"));
+        }
+    }
+}
