076cfbfd12b15d3495ae91083b7d57a88ce07d45
Piotr Lisewski <p.lisewski@hotmail.com>
2021-10-12 22:50:51 +0200

    add TopContainerComponent class

---
+++ SeleniumGridWithDocker/PageObjects/Components/TopContainerComponent.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using SeleniumGridWithDocker.BaseClasses;
+
+namespace SeleniumGridWithDocker.PageObjects.Components
+{
+    class TopContainerComponent : PageBase
+    {
+        private readonly By logoImage = By.ClassName("logo");
+        private readonly By searchForm = By.Id("searchbox");
+        private readonly By shoppingCartLink = By.CssSelector(".shopping_cart > a");
+        private readonly By womenCategoryLink = By.CssSelector("li > [title='Women']");
+        private readonly By dressesCategoryLink = By.XPath("(//a[@title='Dresses'])[2]");
+        private readonly By thirtsCategoryLink = By.XPath("(//a[@title='T-shirts'])[2]");
+
+        public TopContainerComponent(RemoteWebDriver driver) : base(driver)
+        {
+
+        }
+    }
+}
