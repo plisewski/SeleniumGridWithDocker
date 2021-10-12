52525fae9b1f17a646a71b2aef22ec774b434ddf
Piotr Lisewski <p.lisewski@hotmail.com>
2021-10-12 22:27:27 +0200

    add TopNavigationComponent class

---
+++ SeleniumGridWithDocker/PageObjects/Components/TopNavigationComponent.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using SeleniumGridWithDocker.BaseClasses;
+
+namespace SeleniumGridWithDocker.PageObjects.Components
+{
+    class TopNavigationComponent : PageBase
+    {
+        private readonly By signInLink = By.ClassName("login");
+        private readonly By contactLink = By.CssSelector("#contact-link a");
+
+        public TopNavigationComponent(RemoteWebDriver driver) : base(driver)
+        {
+
+        }
+    }
+}
