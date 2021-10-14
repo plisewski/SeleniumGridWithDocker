e57b28057eeb27ed5a7255393f8a9d24853ea4f3
Piotr Lisewski <p.lisewski@hotmail.com>
2021-10-14 23:42:45 +0200

    add DashboardLoggedUserPage class

---
+++ SeleniumGridWithDocker/PageObjects/UserAccountPages/LoggedUserPages/DashboardLoggedUserPage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using SeleniumGridWithDocker.BaseClasses;
+
+namespace SeleniumGridWithDocker.PageObjects.UserAccountPages.LoggedUserPages
+{
+    class DashboardLoggedUserPage : PageBase
+    {
+        private readonly By ordersHistoryLink = By.CssSelector("[title='Orders']");
+        private readonly By creditSlipsLink = By.CssSelector("[title='Credit slips']");
+        private readonly By addressesLink = By.CssSelector("[title='Addresses']");
+        private readonly By personalInformationLink = By.CssSelector("[title='Information']");
+        private readonly By wishlistsLink = By.CssSelector("[title='My wishlists']");
+
+        public DashboardLoggedUserPage(RemoteWebDriver driver) : base(driver)
+        {
+
+        }
+    }
+}
