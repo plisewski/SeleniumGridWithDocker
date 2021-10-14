f509e2caffaa191646bac099509ee3bef044cc52
Piotr Lisewski <p.lisewski@hotmail.com>
2021-10-14 23:32:07 +0200

    add RegistrationPage class

---
+++ SeleniumGridWithDocker/PageObjects/UserAccountPages/RegistrationPage.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium.Remote;
+using SeleniumGridWithDocker.BaseClasses;
+
+namespace SeleniumGridWithDocker.PageObjects.UserAccountPages
+{
+    class RegistrationPage : PageBase
+    {
+        public RegistrationPage(RemoteWebDriver driver) : base(driver)
+        {
+
+        }
+    }
+}
