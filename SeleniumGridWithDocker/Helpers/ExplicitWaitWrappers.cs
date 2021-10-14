cbea6659a6e78154fd06d67f70f509c7a79e03e7
Piotr Lisewski <p.lisewski@hotmail.com>
2021-10-14 23:15:24 +0200

    add WaitHelpers NuGet package and ExplicitWaitWrappers class with implementation of first wait condition

---
+++ SeleniumGridWithDocker/Helpers/ExplicitWaitWrappers.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using System;
+using ExpectedCondition = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+namespace SeleniumGridWithDocker.Helpers
+{
+    public static class ExplicitWaitWrappers
+    {
+        public static void UntilElementToBeClickable(RemoteWebDriver driver, By e, int waitInSeconds = 15)
+        {
+            new WebDriverWait(driver, TimeSpan.FromSeconds(waitInSeconds)).
+                Until(ExpectedCondition.ElementToBeClickable(e));
+        }
+    }
+}
