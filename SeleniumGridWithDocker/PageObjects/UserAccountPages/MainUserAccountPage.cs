7e5bdcc79708d82e94a0d654cd25144249545815
Piotr Lisewski <p.lisewski@hotmail.com>
2021-10-14 23:29:19 +0200

    add MainUserAccountPage class

---
+++ SeleniumGridWithDocker/PageObjects/UserAccountPages/MainUserAccountPage.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using SeleniumGridWithDocker.BaseClasses;
+
+namespace SeleniumGridWithDocker.PageObjects.UserAccountPages
+{
+    class MainUserAccountPage : PageBase
+    {
+        //Registration Form
+        private readonly By registrationEmailTextBox = By.Id("email_create");
+        private readonly By createAccountButton = By.Id("SubmitCreate");
+
+        //Login Form
+        private readonly By loginEmailTextBox = By.Id("email");
+        private readonly By loginPasswordTextBox = By.Id("passwd");
+        private readonly By submitLoginButton = By.Id("SubmitLogin");
+
+        public MainUserAccountPage(RemoteWebDriver driver) : base(driver)
+        {
+
+        }
+    }
+}
