34fecf319de3cc69da46be4b2894e1dad6d573da
Piotr Lisewski <p.lisewski@hotmail.com>
2021-10-16 22:54:14 +0200

    implement methods in RegistrationPage class

--- SeleniumGridWithDocker/PageObjects/UserAccountPages/RegistrationPage.cs
+++ SeleniumGridWithDocker/PageObjects/UserAccountPages/RegistrationPage.cs
@@ -1,6 +1,8 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Remote;
 using SeleniumGridWithDocker.BaseClasses;
+using SeleniumGridWithDocker.Helpers;
+using SeleniumGridWithDocker.PageObjects.UserAccountPages.LoggedUserPages;
 
 namespace SeleniumGridWithDocker.PageObjects.UserAccountPages
 {
@@ -25,6 +27,110 @@ namespace SeleniumGridWithDocker.PageObjects.UserAccountPages
         public RegistrationPage(RemoteWebDriver driver) : base(driver)
         {
 
+        }
+
+        public RegistrationPage FirstNamePersonalTextBox(string text)
+        {
+            ClearTextBox(firstNamePersonalTextBox);
+            FillTextBox(firstNamePersonalTextBox, text);
+
+            return this;
+        }
+
+        public RegistrationPage LastNamePersonalTextBox(string text)
+        {
+            ClearTextBox(lastNamePersonalTextBox);
+            FillTextBox(lastNamePersonalTextBox, text);
+
+            return this;
+        }
+
+        public RegistrationPage EmailTextBox(string text)
+        {
+            ClearTextBox(emailTextBox);
+            FillTextBox(emailTextBox, text);
+
+            return this;
+        }
+
+        public RegistrationPage PasswordTextBox(string text)
+        {
+            ClearTextBox(passwordTextBox);
+            FillTextBox(passwordTextBox, text);
+
+            return this;
+        }
+
+        public RegistrationPage FirstNameAddressTextBox(string text)
+        {
+            ClearTextBox(firstNameAddressTextBox);
+            FillTextBox(firstNameAddressTextBox, text);
+
+            return this;
+        }
+
+        public RegistrationPage LastNameAddressTextBox(string text)
+        {
+            ClearTextBox(lastNameAddressTextBox);
+            FillTextBox(lastNameAddressTextBox, text);
+
+            return this;
+        }
+
+        public RegistrationPage AddressTextBox(string text)
+        {
+            ClearTextBox(addressTextBox);
+            FillTextBox(addressTextBox, text);
+
+            return this;
+        }
+
+        public RegistrationPage CityTextBox(string text)
+        {
+            ClearTextBox(cityTextBox);
+            FillTextBox(cityTextBox, text);
+
+            return this;
+        }
+
+        public RegistrationPage StateTextBox(string text)
+        {
+            ClearTextBox(stateTextBox);
+            FillTextBox(stateTextBox, text);
+
+            return this;
+        }
+
+        public RegistrationPage PostalCodeTextBox(string text)
+        {
+            ClearTextBox(postalCodeTextBox);
+            FillTextBox(postalCodeTextBox, text);
+
+            return this;
+        }
+
+        public RegistrationPage MobilePhoneTextBox(string text)
+        {
+            ClearTextBox(mobilePhoneTextBox);
+            FillTextBox(mobilePhoneTextBox, text);
+
+            return this;
+        }
+
+        public RegistrationPage AddressAliasTextBox(string text)
+        {
+            ClearTextBox(addressAliasTextBox);
+            FillTextBox(addressAliasTextBox, text);
+
+            return this;
+        }
+
+        public DashboardLoggedUserPage ClickSubmitRegistrationButton()
+        {
+            ExplicitWaitWrappers.UntilElementToBeClickable(Driver, submitRegistrationFormButton);
+            ClickOnElement(submitRegistrationFormButton);
+
+            return new DashboardLoggedUserPage(Driver);
         }
     }
 }
