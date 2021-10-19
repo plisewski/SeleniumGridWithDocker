94bc289ea085e890c00d8b5ce6ef839bcde67d17
Piotr Lisewski <p.lisewski@hotmail.com>
2021-10-19 23:42:16 +0200

    add RegistrationTests class

---
+++ SeleniumGridWithDocker/TestScripts/RegistrationTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using SeleniumGridWithDocker.BaseClasses;
+using SeleniumGridWithDocker.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumGridWithDocker.TestScripts
+{
+    [TestFixture]
+    class RegistrationTests : TestBase
+    {
+        [Test]
+        public void CanNotRegisterWithoutProvidingEmailAddress()
+        {
+
+        }
+    }
+}
